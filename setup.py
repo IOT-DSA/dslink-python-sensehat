89de12ba10ac525f70008455494acdeec9f821b1
Logan Gorence <loganjohngorence@gmail.com>
2016-02-10 14:44:37 -0800

    Remove pillow requirement and fix formatting.

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
 
 setup(
-        name="dslink-python-sensehat",
-        version="0.1.0",
-        description="DSLink for Sense HAT on Raspberry Pi",
-        url="http://github.com/IOT-DSA/dslink-python-sensehat",
-        author="Logan Gorence",
-        author_email="lgorence@dglogik.com",
-        license="Apache 2.0",
-        install_requires=[
-            "dslink >=0.6.0, <0.7.0",
-            "pillow",
-            "evdev",
-            "sense_hat"
-        ]
+    name="dslink-python-sensehat",
+    version="0.1.0",
+    description="DSLink for Sense HAT on Raspberry Pi",
+    url="http://github.com/IOT-DSA/dslink-python-sensehat",
+    author="Logan Gorence",
+    author_email="lgorence@dglogik.com",
+    license="Apache 2.0",
+    install_requires=[
+        "dslink >=0.6.0, <0.7.0",
+        "evdev",
+        "sense_hat"
+    ]
 )
