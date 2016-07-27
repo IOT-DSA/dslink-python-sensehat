44c5de93f8b7783cf1a30748da26e77ba25b57ce
Logan Gorence <loganjohngorence@gmail.com>
2016-07-27 13:14:46 -0700

    Update to latest SDK.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name="dslink-python-sensehat",
-    version="0.2.0",
+    version="0.2.1",
     description="DSLink for Sense HAT on Raspberry Pi",
     url="http://github.com/IOT-DSA/dslink-python-sensehat",
     author="Logan Gorence",
     author_email="l.gorence@dglogik.com",
     license="Apache 2.0",
     install_requires=[
-        "dslink == 0.6.6",
+        "dslink == 0.6.16",
         "evdev",
         "sense_hat"
     ]
